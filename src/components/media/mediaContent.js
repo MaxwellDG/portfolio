abead606201ec2124658d14b1d999423e191b4f4
Max Dunk-Green <jajaMundo@protonmail.com>
2022-03-19 14:22:41 -0600

    Done CSS?????

--- src/components/media/mediaContent.js
+++ src/components/media/mediaContent.js
@@ -3,13 +3,13 @@ import React from 'react'
 export default ({image, video}) => {
 
     return(
-        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%', width: '100%'}}>
             {video ?
-                <video className="modal-video" type="video/mp4" controls>
+                <video style={{height: '100%', width: '100%'}} type="video/mp4" controls autoplay>
                     <source src={ video }/>
                 </video>
                 :
-                <img src={ image } alt="img_project" className="modal-image"/>
+                <img src={ image } alt="img_project" style={{height: '100%', width: undefined}}/>
             }
         </div>
     )
