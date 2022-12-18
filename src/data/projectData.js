2c74e0c20090819cc843d26e2801def99f616580
Maxwell Dunk-Green <mdgreen@viihealth.com>
2022-12-18 14:45:01 -0500

    Updates

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -1,10 +1,21 @@
 export const mobileProjectData = [
     {
+        projectName: "SMART Studio",
+        link: undefined,
+        linkText: 'SMART Studio on The App Store',
+        description: "Designed for medical companies to generate informative videos for their workers and patients. Utilizes Deepfake technology in order to quickly mass-produce video content of doctors speaking to patients in any language. AI support bots currently in development in order to automatically generate deepfake videos in response to textual input.",
+        video: 'videos/smart-studio-ready.mp4',
+        language: "React Native", 
+        tech: "Deepfakes, GraphQL, AWS, Expo",
+        videoClass: "videos",
+        projectClass: "mobileProjectSection",
+        isNDA: true
+    },    
+    {
         projectName: "The Diabetes App",
-        floatSide: "left",
         link: "https://apps.apple.com/app/apple-store/id1522262547",
         linkText: 'The Diabetes App on The App Store',
-        description: "A social media app focused around people with diabetes. Users can make profiles, message each other, scroll the homefeed, there's app-wide video streaming events, and much more. I created v2 of this product and at the moment I'm setup to create v3 as well.",
+        description: "A social media app focused around people with diabetes. Users can make profiles, message each other, scroll the homefeed, there's app-wide video streaming events, and much more.",
         image: 'images/tda.png',
         language: "React Native", 
         tech: "Oauth2, Pusher, Mux, Kevel",
@@ -14,7 +25,6 @@ export const mobileProjectData = [
     },    
     {
         projectName: "ImgurCreepur",
-        floatSide: "right",
         link: "https://github.com/MaxwellDG/ImgurCreepur",
         description: "Imgur is Reddit's main image hosting website. Users can creep others' profiles and activities within simplified UI.",
         video: "videos/creepur1final.mp4",
@@ -26,7 +36,6 @@ export const mobileProjectData = [
     },    
     {
         projectName: "WithWtvr",
-        floatSide: "left",
         link: "https://github.com/MaxwellDG/WithWtvr",
         description: "\"Hey where do you want to go tonight?\" \"I'm fine with whatever.\" An app designed to aid groups in decision-making. Utilizes Google Maps and Google Places APIs for extra suggestions.",
         video: "videos/video_ww_right_final.mp4",
@@ -41,11 +50,11 @@ export const mobileProjectData = [
 export const otherProjectData = [
     {
         projectName: "The Bard's Quill",
-        floatSide: "right",
         link: "https://thebardsquill.com",
         linkText: 'TheBardsQuill.com',
-        description: "A virtual tabletop and campaign-management system for the 'Storytelling Role-Playing Game' genre. The Bard's Quill works as an aide for popular titles such as: 'Dungeons & Dragons' and 'Pathfinder'. It is currently in alpha stage and will be released to the public Spring 2022.",
-        image: "images/bardsquill.png",
+        description: "A virtual tabletop and campaign-management system for the 'Storytelling Role-Playing Game' genre. The Bard's Quill works as an aide for popular titles such as: 'Dungeons & Dragons' and 'Pathfinder'.",
+        video: 'videos/thebardsquill_cropped.mp4',
+        image: 'images/bardsquill.png',
         language: "Vue.js", 
         tech: "WebRTC, Konva.js, IndexedDB",
         videoClass: "videosOther",
@@ -53,13 +62,24 @@ export const otherProjectData = [
         isNDA: true
     },
     {
+        projectName: "Luye",
+        link: undefined,
+        linkText: '- Currently in development -',
+        description: "An offline-first Progressive Web App used by pharmaceutical sales reps during their meetings with health-care professionals. Utilizes ServiceWorkers and IndexedDB in order to persist data in offline-mode and sync it with the server when an internet connection is later established.",
+        image: "images/Luye.png",
+        language: "React-Native-Web", 
+        tech: "ServiceWorkers, IndexedDB",
+        videoClass: "videosOther",
+        projectClass: "otherProjectSection",
+        isNDA: true
+    },
+    {
         projectName: "Duel Arena",
-        floatSide: "left",
         link: "https://github.com/MaxwellDG/duel_arena",
         description: "A crypto gambling website. Instead of users having to take bad odds due to casinos taking a cut, they can bet directly against each other with 50/50 -> 2x odds.",
         subText: '* This is a personal project done for the purpose of learning Blockchain and is currently in development',
         image: "images/duelarena.png",
-        language: "Solidity, Vue.js", 
+        language: "Solidity, Rust, Vue.js", 
         tech: "Truffle, ChainLink, SkyNet",
         videoClass: "videosOther",
         projectClass: "otherProjectSection",
@@ -67,10 +87,10 @@ export const otherProjectData = [
     },
     {
         projectName: "Lucky13 Tattoos",
-        floatSide: "right",
         link: "https://github.com/MaxwellDG/lucky13",
         description: "Website for the Toronto tattoo parlour. Static plus an e-commerce section.",
         video: "videos/lucky13.mp4",
+        image: "images/lucky13.png",
         language: "React.js, Node.js", 
         tech: "Stripe, MailGun, Node.js, AWS",
         videoClass: "videosOther",
@@ -78,23 +98,11 @@ export const otherProjectData = [
         isNDA: false
     },
     {
-        projectName: "Homebase",
-        floatSide: "left",
-        link: "https://github.com/MaxwellDG/homebase",
-        description: "A homepage that is meant to increase productivity. There's no clutter, and instead there are groupings of customizable websites to expedite pre-determined tasks.",
-        video: "videos/homepage-video.mp4",
-        language: "React.js, Node.js", 
-        tech: "Node.js, MongoDB, AWS",
-        videoClass: "videosOther",
-        projectClass: "otherProjectSection",
-        isNDA: false
-    },
-    {
         projectName: "CSA-Scraper",
-        floatSide: "right",
         link: "https://github.com/MaxwellDG/CSA_Scraper",
         description: "A scraper for a client who wanted to track the status of Canadian securities administrators. 'Scrapes' the Canadian Securities Administrators Registry and produces a .csv after comparisons are made.",
         video: "videos/CSA_Video.mp4",
+        image: "images/csa_scraper.png",
         language: "Python", 
         tech: "Selenium, TkInter, BeautifulSoup4, MS Excel",
         videoClass: "videosOther",
@@ -103,10 +111,10 @@ export const otherProjectData = [
     },
     {
         projectName: "PyGame",
-        floatSide: "left",
         link: "https://github.com/MaxwellDG/python-pygame",
         description: "A videogame made in similar style to the classic 'The Legend of Zelda'. Defeat monsters, collect upgrades, and defeat the evil Mecha-Boss C++9000",
         video: "videos/Python_Vid.mp4",
+        image: "images/pygame.png",
         language: "Python", 
         tech: "PyGame, Photoshop",
         videoClass: "videosOther",
