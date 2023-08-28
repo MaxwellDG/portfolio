e73d26df75b4a83e5f12fa3f86535812e5d96f3e
Maxwell DG <maxwellmdg@gmail.com>
2023-08-28 12:41:54 -0400

    Updated resume

--- middleware.ts
+++
@@ -1,8 +0,0 @@
-import { NextRequest, NextResponse } from 'next/server';
-// import { connectToDb } from './db';
-
-export default async function middleware(req: NextRequest) {
-  console.log("Middleware triggaed")
-  // await connectToDb();
-  return NextResponse.next();
-}
