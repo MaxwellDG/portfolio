ace8419c2975f2aefe471b832c01607790d4d4e1
Maxwell <maxwellmdg@protonmail.com>
2023-11-20 11:50:44 -0500

    Added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import '../theme/globals.css';
 import '../theme/app.scss';
 import '../theme/app.scss';
 import BackgroundAnimation from '../components/backgroundAnimation';
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata = {
     title: 'Maxwell Dunk-Green',
@@ -21,13 +22,14 @@ export default function RootLayout({
     return (
         <html lang="en" className="flex h-full">
             <body className="flex flex-1 relative pb-[43px] lg:pb-0">
-                <BackgroundAnimation />
-                {/* <ThemeProvider> */}
-                <ScreenProvider>
-                    {children}
-                    <MobileNav />
-                </ScreenProvider>
-                {/* </ThemeProvider> */}
+                    <BackgroundAnimation />
+                    {/* <ThemeProvider> */}
+                    <ScreenProvider>
+                        {children}
+                        <MobileNav />
+                    </ScreenProvider>
+                    {/* </ThemeProvider> */}
+                <Analytics />
             </body>
         </html>
     );
