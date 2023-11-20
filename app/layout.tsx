cf14f882e0f70c75986fbaf65541055fdfdc0040
Maxwell <maxwellmdg@protonmail.com>
2023-11-20 11:54:02 -0500

    Added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
                         <MobileNav />
                     </ScreenProvider>
                     {/* </ThemeProvider> */}
-                <Analytics />
+                    <Analytics />
             </body>
         </html>
     );
